ff525f89476bbed633c81742a47371742a827f62
najaeseong <skwotjd5576@gmail.com>
2023-09-15 17:45:20 +0900

    2023-09-15 spring-ractice

---
+++ core/src/main/java/core/main/Main2.java
@@ -0,0 +1,20 @@
+package core.main;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import core.chap3.Hotel;
+
+public class Main2 {
+	
+	public static void main(String[] args) {
+
+		// 빈 등록에 사용된 xml의 설정을 가져오는 객체를 생성. (classpath == src/main/resources)
+		GenericXmlApplicationContext ct = 
+				new GenericXmlApplicationContext("classpath:auto-config.xml");
+		
+		Hotel hotel1 = ct.getBean("hotel1", Hotel.class);
+		hotel1.inform();
+		
+	}
+
+}
